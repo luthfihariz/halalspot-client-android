4d622096d68699178f1fa8b41d3463f3b220a5cc
Luthfi Hariz <lavalyst@gmail.com>
2014-01-19 16:29:11 +0700

    get direction & map in place detail activity

--- src/com/sharee/halalspot/activities/PlaceDetailActivity.java
+++ src/com/sharee/halalspot/activities/PlaceDetailActivity.java
@@ -3,11 +3,14 @@ package com.sharee.halalspot.activities;
 import java.util.ArrayList;
 import java.util.Locale;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -15,6 +18,11 @@ import android.widget.TextView;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Window;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.sharee.halalspot.R;
 import com.sharee.halalspot.adapters.PhotoPagerAdapter;
 import com.sharee.halalspot.beans.Photo;
@@ -26,6 +34,8 @@ import com.sharee.utilities.OnAsyncTaskCompleted;
 public class PlaceDetailActivity extends SherlockFragmentActivity {
 
 	private Intent intent;
+	private Button getDirectionBtn;
+	private Button saveBtn;
 	private TextView placeName;
 	private TextView placeCat;
 	private TextView placeAddr;
@@ -38,20 +48,30 @@ public class PlaceDetailActivity extends SherlockFragmentActivity {
 	private RelativeLayout webBorder;
 	private RelativeLayout callContainer;
 	private RelativeLayout callBorder;
+	private GoogleMap placeMap;
+
+	private double latitude;
+	private double longitude;
+	private double userLatitude;
+	private double userLongitude;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		intent = getIntent();
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		initView();
 		initBackgroundTask();
+		initMap();
+		initAction();
 	}
 
 	private void initView() {
 		setContentView(R.layout.activity_place_detail);
 		setSupportProgressBarIndeterminateVisibility(true);
 
+		getDirectionBtn = (Button) findViewById(R.id.btn_get_direction);
+		saveBtn = (Button) findViewById(R.id.btn_save);
 		placeName = (TextView) findViewById(R.id.txt_name);
 		placeAddr = (TextView) findViewById(R.id.txt_address);
 		placeCat = (TextView) findViewById(R.id.txt_category);
@@ -76,6 +96,49 @@ public class PlaceDetailActivity extends SherlockFragmentActivity {
 				+ intent.getStringExtra(Helper.KEY_PLACE_COUNTRY));
 	}
 
+	private void initAction() {
+		getDirectionBtn.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(Intent.ACTION_VIEW, Uri
+						.parse("http://maps.google.com/maps?f=d&daddr="
+								+ latitude + "," + longitude));
+				intent.setComponent(new ComponentName(
+						"com.google.android.apps.maps",
+						"com.google.android.maps.MapsActivity"));
+				startActivity(intent);
+			}
+		});
+
+		saveBtn.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+	}
+
+	private void initMap() {
+		placeMap = ((SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.map_place)).getMap();
+		placeMap.getUiSettings().setZoomControlsEnabled(false);
+		placeMap.getUiSettings().setAllGesturesEnabled(false);
+		placeMap.getUiSettings().setCompassEnabled(false);
+
+		latitude = intent.getDoubleExtra(Helper.KEY_PLACE_LAT, 0.0);
+		longitude = intent.getDoubleExtra(Helper.KEY_PLACE_LNG, 0.0);
+
+		MarkerOptions options = new MarkerOptions();
+		LatLng latLng = new LatLng(latitude, longitude);
+		options.position(latLng);
+		placeMap.addMarker(options);
+		placeMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
+	}
+
 	private void initBackgroundTask() {
 		String placeId = intent.getStringExtra(Helper.KEY_PLACE_ID);
 		Helper.log(placeId);
@@ -83,12 +146,11 @@ public class PlaceDetailActivity extends SherlockFragmentActivity {
 				new OnAsyncTaskCompleted() {
 
 					@Override
-					public void onCompleted(boolean status,
-							Object... objects) {
+					public void onCompleted(boolean status, Object... objects) {
 						setSupportProgressBarIndeterminateVisibility(false);
 						if (status) {
 							Place place = (Place) objects[0];
-							updateView(place);
+							updateViewAfterGetDetail(place);
 						} else {
 							String msg = (String) objects[0];
 							Helper.toastShort(PlaceDetailActivity.this, msg);
@@ -98,21 +160,45 @@ public class PlaceDetailActivity extends SherlockFragmentActivity {
 		getPlaceDetail.execute(placeId);
 	}
 
-	private void updateView(Place place) {
+	private void updateViewAfterGetDetail(Place place) {
 		setPhotos(place.getPhotos());
-		if(!place.getWebsite().equals("")){
+		if (!place.getWebsite().equals("")) {
 			placeWeb.setText(place.getWebsite());
 			webContainer.setVisibility(View.VISIBLE);
 			webBorder.setVisibility(View.VISIBLE);
-		}		
-		if(!place.getPhone().equals("")){
-			placePhone.setText(place.getPhone());		
+			setWebsiteListener();
+		}
+		if (!place.getPhone().equals("")) {
+			placePhone.setText(place.getPhone());
 			callContainer.setVisibility(View.VISIBLE);
 			callBorder.setVisibility(View.VISIBLE);
-		}		
+			setCallListener();
+		}
 	}
 
-	private void setPhotos(ArrayList<Photo> photos) {		
+	private void setCallListener() {
+		callContainer.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+	}
+
+	private void setWebsiteListener() {
+		webContainer.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+	}
+
+	private void setPhotos(ArrayList<Photo> photos) {
 		PhotoPagerAdapter adapter = new PhotoPagerAdapter(
 				getSupportFragmentManager(), photos);
 		photosPager.setAdapter(adapter);
